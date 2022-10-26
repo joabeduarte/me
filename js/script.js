bec9c084bcb90eea810ada0ecfeac9920fa771da
Joabe <joabeduarte.br@gmail.com>
2022-10-25 21:34:49 -0300

    adding js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// currículo
+const curriculo = document.querySelector(".curriculo");
+
+curriculo.addEventListener("click", () => {
+  alert("Solicitar currículo via e-mail ou rede social");
+});
+
+// react
+const react = document.querySelector(".react");
+
+react.addEventListener("click", () => {
+  alert("Certificado não disponível: Curso em andamento");
+});
